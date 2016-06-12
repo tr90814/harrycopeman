e9b7ff41686e1120b406413946cb120bf618a7dd
Tom Rogers <tomrogers2823@gmail.com>
2016-06-12 16:48:46 +0100

    Address map

--- server/main.js
+++ server/main.js
@@ -4,5 +4,7 @@ import { ForwardingMap } from '../imports/api/forwarding_map';
 import { Meteor } from '../imports/startup/meteor_context';
 
 Meteor.startup(() => {
-  console.log('[MAP]', ForwardingMap.find().fetch());
+  console.log('[MAP]: inboundAddress => forwardingAddress\n', ForwardingMap.find().fetch().map((el) => {
+    return el.inboundAddress + ' => ' + el.forwardingAddress;
+  }).join('\n'));
 });
