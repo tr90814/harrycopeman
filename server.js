3294051d31679a2d6f533d6f24de141721c157e0
Tom Rogers <tomrogers2823@gmail.com>
2017-02-03 18:30:03 +0000

    Initial commit

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
 const express    = require('express');
-const bodyParser = require('body-parser');
-const morgan     = require('morgan');
-const mongoose   = require('mongoose');
-const router     = require('./startup/routes');
-const ENV        = require('./startup/env');
-const auth       = require('./startup/middleware').auth;
 
 // App init
 const app = express();
@@ -12,22 +6,7 @@ const app = express();
 // Port
 const port = process.env.PORT || 8080;
 
-// Logging for dev
-app.use(morgan('dev'));
-
-// Configure body parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use('/api/mail_maps', auth);
-
-// Mongoose setup
-mongoose.connect(ENV.MONGODB_URI);
-
-// Add routes
-app.use('/api', router);
-
-// Redirect
-app.get('/', (req, res) => res.redirect('https://farewill.com'));
+app.get('*', (req, res) => res.send('<h1>Hello world</h1>'));
 
 // Listen
 app.listen(port);
